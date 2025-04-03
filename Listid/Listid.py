4b4cbc98849336be39f062555408ffbfad020105
ReginaKolesnikova <151017512+ReginaKolesnikova@users.noreply.github.com>
2025-04-03 16:06:14 +0300

    Add files via upload

---
+++ Listid/Listid.py
@@ -0,0 +1,98 @@
+spisok=[] #pustoy spisok 
+numbers=[1,2,3,4,5]
+abc=['Abc','B','C']
+slovo="Programmeerimine"
+slovo_list=list(slovo)
+abc_list=list(abc)
+numbers_list=list(numbers)
+
+# print(slovo)
+# print(slovo_list)
+
+while True:
+  print("1-repeat string\n2-find letter by i- position") #s1 3 #s i
+  print("3-get certain part of list \n4-show string length") #s i j step # len s
+  print("5-find first encounter\n6-split list with symbol") #s find str start end # s split
+  print("7-find out whether there is only letters in string or not \n8-makes caps letters") # s isalpha # s upper
+  print("9-captalize only first leter of word in string\n10-swap uppercase letters with lowercase ones ") # s capitalize # s swapcase
+
+
+  #S-spisok
+  #i-position
+  
+  valik=int(input("Choose operation: "))
+  if valik==1: #repeat string
+      a=int(input("How much repeats: "))
+      repe=numbers_list*a
+      print (f"Repeated {a} times: ", repe)
+  elif valik==2: #find letter by i- position
+    a=int(input("Enter position number (0,1,2,3,4...): "))
+    n=slovo_list[a]
+    print(n)
+  elif valik==3: #get certain part of list
+    a=int(input("Enter first position (0,1,2,3...): "))
+    b=int(input("Enter last position (0,1,2,3...): "))
+    c=int(input("Enter step length (0,1,2,3...)"))
+    n=slovo_list[a:b:c]
+    print(n)
+  elif valik==4: #show string length
+       a=int(input("Choose list to count its length (1-word, 2-letters or 3-numbers): "))
+       if a==1:
+          b=len(slovo_list)
+          print(f"In list {slovo_list} - {b} symbols")
+       elif a==2:
+          b=len(abc_list)
+          print(f"In list {abc_list} - {b} symbols")
+       elif a==3:
+          b=len(numbers_list)
+          print(f"In list {numbers_list} - {b} symbols")
+       else:
+           print("wrong number")
+  elif valik==5: #find first encounter
+      stirt=input("hn")
+      nd=input("ddd")
+      ngd=input("ddd")
+      slovo_list.find(stirt,nd)
+      print(slovo_list)
+
+  # elif valik==6:
+
+  # # S.find(str, [start],[end])
+  #   a=input("uuu")
+  #   i=int(input(": "))
+  #   slovo_list.insert(i-1,a) #0,1,2...
+  #   print(slovo_list)
+
+
+  # elif valik==7:
+  # elif valik==8:
+  # elif valik==9:
+  # elif valik==10:
+  # else:
+  #    print("There is no such letter, try enter again")
+  # print(slovo_list)
+
+
+
+  # valik=int(input())
+  # if valik==1:
+  #    a=input("Enter letter: ")
+  #    slovo_list.append(a)
+  #    print (f"Added {a} in new list", slovo_list)
+  # elif valik==2:
+  #   slovo_list.extend(abc)
+  #   print(slovo_list)
+  # elif valik==3:
+  #   a=input("Enter letter, which you want to add: ")
+  #   i=int(input("Enter number of position, where you want to add letter: "))
+  #   slovo_list.insert(i-1,a) #0,1,2...
+  #   print(slovo_list)
+  # elif valik==4:
+  #   a=input("Enter letter, which you want to delete: ")
+  #   n=slovo_list.count(a)
+  #   if n>0:
+  #      for i in range(n):
+  #        slovo_list.remove(a)
+  # else:
+  #        print("There is no such letter, try again")
+  # print(slovo_list)
